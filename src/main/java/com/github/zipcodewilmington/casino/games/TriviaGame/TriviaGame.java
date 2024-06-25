7e6c3cec1ba3aef49f9a33c3bcb6e19a3bd7f4ae
moffd234 <159962112+moffd234@users.noreply.github.com>
2024-06-25 13:58:52 -0400

    Make TriviaGame Class

---
+++ src/main/java/com/github/zipcodewilmington/casino/games/TriviaGame/TriviaGame.java
@@ -0,0 +1,26 @@
+package com.github.zipcodewilmington.casino.games.TriviaGame;
+
+import com.github.zipcodewilmington.casino.GameInterface;
+import com.github.zipcodewilmington.casino.PlayerInterface;
+
+public class TriviaGame implements GameInterface {
+    @Override
+    public void add(PlayerInterface player) {
+        
+    }
+
+    @Override
+    public void remove(PlayerInterface player) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public String printWelcomeMessage() {
+        return "";
+    }
+}
