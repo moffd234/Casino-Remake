1edaabc48e5c2834a9df07d07648b34521f447cc
moffd234 <159962112+moffd234@users.noreply.github.com>
2024-07-01 11:08:07 -0400

    Added printWelcomeMessage

--- src/test/java/com/github/zipcodewilmington/GameTest/Trivia/TriviaGameTest.java
+++ src/test/java/com/github/zipcodewilmington/GameTest/Trivia/TriviaGameTest.java
@@ -1,11 +1,17 @@
 package com.github.zipcodewilmington.GameTest.Trivia;
 
 import com.github.zipcodewilmington.casino.games.TriviaGame.TriviaGame;
-import okhttp3.Response;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TriviaGameTest {
+    TriviaGame game;
+
+    @Before
+    public void setUp(){
+        game = new TriviaGame();
+    }
 
     @Test
     public void testGetQuestionResponse(){
@@ -14,5 +20,20 @@ public class TriviaGameTest {
 
         boolean actual = response.contains("response_code");
         Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void testPrintWelcomeMessage() {
+        String expected = " /$$      /$$ /$$$$$$$$ /$$        /$$$$$$   /$$$$$$  /$$      /$$ /$$$$$$$$       /$$$$$$$$ /$$$$$$        /$$$$$$$$ /$$$$$$$  /$$$$$$ /$$    /$$ /$$$$$$  /$$$$$$ \n" +
+                "| $$  /$ | $$| $$_____/| $$       /$$__  $$ /$$__  $$| $$$    /$$$| $$_____/      |__  $$__//$$__  $$      |__  $$__/| $$__  $$|_  $$_/| $$   | $$|_  $$_/ /$$__  $$\n" +
+                "| $$ /$$$| $$| $$      | $$      | $$  \\__/| $$  \\ $$| $$$$  /$$$$| $$               | $$  | $$  \\ $$         | $$   | $$  \\ $$  | $$  | $$   | $$  | $$  | $$  \\ $$\n" +
+                "| $$/$$ $$ $$| $$$$$   | $$      | $$      | $$  | $$| $$ $$/$$ $$| $$$$$            | $$  | $$  | $$         | $$   | $$$$$$$/  | $$  |  $$ / $$/  | $$  | $$$$$$$$\n" +
+                "| $$$$_  $$$$| $$__/   | $$      | $$      | $$  | $$| $$  $$$| $$| $$__/            | $$  | $$  | $$         | $$   | $$__  $$  | $$   \\  $$ $$/   | $$  | $$__  $$\n" +
+                "| $$$/ \\  $$$| $$      | $$      | $$    $$| $$  | $$| $$\\  $ | $$| $$               | $$  | $$  | $$         | $$   | $$  \\ $$  | $$    \\  $$$/    | $$  | $$  | $$\n" +
+                "| $$/   \\  $$| $$$$$$$$| $$$$$$$$|  $$$$$$/|  $$$$$$/| $$ \\/  | $$| $$$$$$$$         | $$  |  $$$$$$/         | $$   | $$  | $$ /$$$$$$   \\  $/    /$$$$$$| $$  | $$\n" +
+                "|__/     \\__/|________/|________/ \\______/  \\______/ |__/     |__/|________/         |__/   \\______/          |__/   |__/  |__/|______/    \\_/    |______/|__/  |__/";
+        String actual = game.printWelcomeMessage();
+
+        Assert.assertEquals(expected, actual);
     }
 }
