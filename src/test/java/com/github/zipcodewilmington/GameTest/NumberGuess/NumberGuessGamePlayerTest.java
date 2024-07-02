dcaaf7af089c77e22f2daa439287b12c8ae46d03
moffd234 <159962112+moffd234@users.noreply.github.com>
2024-07-02 11:46:03 -0400

    add getAccount test for NumberGuessPlayer

--- src/test/java/com/github/zipcodewilmington/GameTest/NumberGuess/NumberGuessGamePlayerTest.java
+++ src/test/java/com/github/zipcodewilmington/GameTest/NumberGuess/NumberGuessGamePlayerTest.java
@@ -2,11 +2,22 @@ package com.github.zipcodewilmington.GameTest.NumberGuess;
 
 import com.github.zipcodewilmington.casino.CasinoAccount;
 import com.github.zipcodewilmington.casino.PlayerInterface;
+import com.github.zipcodewilmington.casino.games.TriviaGame.TriviaGamePlayer;
 import com.github.zipcodewilmington.casino.games.numberguess.NumberGuessPlayer;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class NumberGuessGamePlayerTest {
+    @Test
+    public void testGetAccount() {
+        CasinoAccount expected = new CasinoAccount("tester","tester");
+        NumberGuessPlayer player = new NumberGuessPlayer(expected);
+
+        CasinoAccount actual = player.getArcadeAccount();
+
+        Assert.assertEquals(expected, actual);
+    }
+
     @Test
     public void testIsInstanceOfGameInterface(){
         NumberGuessPlayer player = new NumberGuessPlayer(new CasinoAccount("tester","tester"));
