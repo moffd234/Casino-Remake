91efbcc21a4b1cac9c9b864fb41178de53ada40f
Daniel Moffett <moffd234@gmail.com>
2024-07-17 14:18:38 -0400

    Added CoinFlipGame and CoinFlipPlayer

---
+++ src/main/java/com/github/zipcodewilmington/casino/games/CoinFlip/CoinFlipPlayer.java
@@ -0,0 +1,4 @@
+package com.github.zipcodewilmington.casino.games.CoinFlip;
+
+public class CoinFlipPlayer {
+}
---
+++ src/test/java/com/github/zipcodewilmington/GameTest/CoinFlip/CoinFlipGameTest.java
@@ -0,0 +1,4 @@
+package com.github.zipcodewilmington.GameTest.CoinFlip;
+
+public class CoinFlipGameTest {
+}
