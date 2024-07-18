716048397496d7f44b35bd170ffa55c0cf885c7e
Daniel Moffett <moffd234@gmail.com>
2024-07-17 23:54:20 -0400

    Made coinFlipPlayer and coinFlipGame implement their respective interfaces

--- src/main/java/com/github/zipcodewilmington/casino/games/CoinFlip/CoinFlipGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/CoinFlip/CoinFlipGame.java
@@ -1,4 +1,26 @@
 package com.github.zipcodewilmington.casino.games.CoinFlip;
 
-public class CoinFlipGame {
+import com.github.zipcodewilmington.casino.GamblingGameInterface;
+import com.github.zipcodewilmington.casino.PlayerInterface;
+
+public class CoinFlipGame implements GamblingGameInterface {
+    @Override
+    public void add(PlayerInterface player) {
+
+    }
+
+    @Override
+    public void remove(PlayerInterface player) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public String printWelcomeMessage() {
+        return "";
+    }
 }
--- src/main/java/com/github/zipcodewilmington/casino/games/CoinFlip/CoinFlipPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/CoinFlip/CoinFlipPlayer.java
@@ -1,4 +1,26 @@
 package com.github.zipcodewilmington.casino.games.CoinFlip;
 
-public class CoinFlipPlayer {
+import com.github.zipcodewilmington.casino.CasinoAccount;
+import com.github.zipcodewilmington.casino.GamblingPlayerInterface;
+
+public class CoinFlipPlayer implements GamblingPlayerInterface {
+    @Override
+    public int wager() {
+        return 0;
+    }
+
+    @Override
+    public int updateAccount(int amount) {
+        return 0;
+    }
+
+    @Override
+    public CasinoAccount getArcadeAccount() {
+        return null;
+    }
+
+    @Override
+    public <SomeReturnType> SomeReturnType play() {
+        return null;
+    }
 }
