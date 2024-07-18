332c028e9b8dcfdab58893cc13ac17fb01a85416
Daniel Moffett <moffd234@gmail.com>
2024-07-18 00:08:30 -0400

    Wrote inheritance test for CoinFlipGame and CoinFlipPlayer

--- src/test/java/com/github/zipcodewilmington/GameTest/CoinFlip/CoinFlipGameTest.java
+++ src/test/java/com/github/zipcodewilmington/GameTest/CoinFlip/CoinFlipGameTest.java
@@ -1,4 +1,24 @@
 package com.github.zipcodewilmington.GameTest.CoinFlip;
 
+import com.github.zipcodewilmington.casino.GamblingGameInterface;
+import com.github.zipcodewilmington.casino.GameInterface;
+import com.github.zipcodewilmington.casino.games.CoinFlip.CoinFlipGame;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class CoinFlipGameTest {
+    CoinFlipGame game;
+
+
+    @Before
+    public void setup(){
+        game = new CoinFlipGame();
+    }
+
+    @Test
+    public void testInheritance(){
+        Assert.assertTrue(game instanceof GameInterface);
+        Assert.assertTrue(game instanceof GamblingGameInterface);
+    }
 }
--- src/test/java/com/github/zipcodewilmington/GameTest/CoinFlip/CoinFlipPlayerTest.java
+++ src/test/java/com/github/zipcodewilmington/GameTest/CoinFlip/CoinFlipPlayerTest.java
@@ -1,4 +1,23 @@
 package com.github.zipcodewilmington.GameTest.CoinFlip;
 
+import com.github.zipcodewilmington.casino.GamblingPlayerInterface;
+import com.github.zipcodewilmington.casino.PlayerInterface;
+import com.github.zipcodewilmington.casino.games.CoinFlip.CoinFlipPlayer;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class CoinFlipPlayerTest {
+    CoinFlipPlayer player;
+
+    @Before
+    public void setUp() {
+        player = new CoinFlipPlayer();
+    }
+
+    @Test
+    public void testInheritance(){
+        Assert.assertTrue(player instanceof PlayerInterface);
+        Assert.assertTrue(player instanceof GamblingPlayerInterface);
+    }
 }
